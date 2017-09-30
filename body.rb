8503a17865fbe3d36d8326a81be570dd59fcf79f
Lan Phan <lanc_phan@hotmail.com>
2017-09-30 16:16:49 -0400

    created a 'body' to initialize the field_name and hectare size

---
+++ body.rb
@@ -0,0 +1,16 @@
+class Body
+
+  def initialize(field_name, hectare_size)
+    @field_name = field_name
+    @hectare_size = hectare_size
+  end
+
+  def field_name
+    @field_name
+  end
+
+  def hectare_size
+    @hectare_size
+  end
+
+end
